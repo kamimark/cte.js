7e4fd756150693566312f37ed79afa1fd70d1b0b
kamimark <kamimark@gmail.com>
2018-05-02 22:51:05 +0300

    pre-cleanup

--- cte.debug.js
+++ cte.debug.js
@@ -1,6 +1,21 @@
 'use strict';
 
 (() => {
+  const THIS = 'this';
+  const OPTIONS = 'cte-options';
+  const OPTIONS_SELECTOR = '[cte-options]';
+  const DATA = 'data';
+  const DATA_SELECTOR = '[data]';
+  const DATA_TYPE = 'data-type';
+  const DATA_TYPE_SELECTOR = '[data-type]';
+  const CTE = 'cte';
+  const CTE_SELECTOR = '[cte]';
+  const CTE_REF = 'cte-ref';
+  const ATTRS_SELECTOR = '[attrs]';
+  const ATTRS = 'attrs';
+  const ANONYMOUS_CTE_START = 'anonymous_cte_template_';
+
+  // DOM helpers
   function ready(fn) {
     if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
       fn();
@@ -9,98 +24,41 @@
     }    
   }
 
-  function createObject(fullPath, root) {
-    const paths = fullPath.split('.');
-    for (const path of fullPath) {
-      if (!root[path]) {
-        root[path] = {};
-      }
-      root = root[path];
-    }
-
-    return root;
+  function removeChildren(el) {
+    while (el.lastChild)
+      el.removeChild(el.lastChild);
   }
 
-  function getObjectValue(obj, dataPath) {
-    if (dataPath == "this")
-      return obj;
-
-    let value = obj;
-    for (const path of dataPath.split('.')) {
-      value = value[path];
-    }
-
-    return value;
-  }
-  
-  const specialInputPopulators = {
-    get_number(el, value) {
-      if (value === null || value === undefined)
-        value = el.getAttribute('defaultValue');
-      if (value === null || value === undefined)
-        value = el.getAttribute('min');
-      return value === null || value === undefined ? '' : value;
-    },
-    checkbox(el, value) {
-      el.checked = (value && value != "false");
-    },
-    number(el, value) {
-      el.value = this.get_number(el, value);
-    },
-    range(el, value) {
-      value = this.get_number(el, value);
-      el.value = value ? value : 0;
-    }
-  }
-  
-  const specialPopulators = {
-    INPUT(el, value) {
-      const type = el.getAttribute('type');
-      if (type in specialInputPopulators)
-        specialInputPopulators[type](el, value);
-      else
-        el.value = value;
-    },
-    SELECT(el, value) {
-      el.value = value;
-    }
-  }
-  
-  function populateValue(el, value) {
-    if (el.tagName in specialPopulators)
-      specialPopulators[el.tagName](el, value);
-    else
-      el.innerHTML = value ? value : '';
-  }
-
-  function populateAttributes(el, attributes) {
+  function setAttributes(el, attributes) {
     for (const attributeName in attributes)
-      if (attributes[attributeName])
-        el.setAttribute(attributeName, attributes[attributeName]);
+      el.setAttribute(attributeName, attributes[attributeName]);
   }
 
-  function populateInnerTemplate(container, objects) {
-    const ref = container.getAttribute('cte-ref');
-    if (ref)
-      for (const obj of objects)
-        newDomUsingTemplateId(ref, container, obj);
-    else
-      populateAnonymousTemplate(container, objects);
+  function populateObject(object, pathAttribute, value) {
+    const paths = pathAttribute.split('.');
+
+    let key = paths.pop();
+
+    if (!paths.length)
+      return value;
+
+    const last = resolveSafe(object, paths);
+    last[key] = value;
   }
-  
-  function populateAnonymousTemplate(container, objects) {
-    if (!container.anonymousTemplateChildren) {
-      container.anonymousTemplateChildren = Array.from(container.children);
-      for (const el of container.anonymousTemplateChildren) {
-        container.removeChild(el);
-      }
-    }
+/*
+  const globalOptions = {};
+  function setDynamicOptions(id, options) {
+    globalOptions[id] = options; 
+  }
 
-    if (container.tagName == "SELECT") {
-      for (const i = 0; i < container.length; ++i)
-        container.remove(i);
+  function buildDynamicOptions(selectEl, optionsId) {
+    if (optionsId in globalOptions) {
+      const options = globalOptions[optionsId];
 
-      for (const option of objects) {
+      for (let i = 0; i < selectEl.length; ++i)
+        selectEl.remove(i);
+
+      for (const option of options) {
         const element = option.element ? option.element : 'option';
         const o = document.createElement(element);
         for (const key of Object.keys(option))
@@ -110,57 +68,244 @@
             else
               o.setAttribute(key, option[key]);
 
-        container.add(o);
+        selectEl.add(o);
       }
     } else {
-      for (const i in objects) {
-        for (const child of container.anonymousTemplateChildren) {
-          const freshChild = child.cloneNode(true);
-          put(freshChild, objects[i]);
-          container.appendChild(freshChild);
+      console.error(`Options with id ${optionsId} was not defined`);
+    }
+  }
+*/
+  function resolveSafe(obj, paths) {
+    return paths.reduce((prev, curr) => {
+        if (!prev || !(curr in prev))
+          return undefined;
+        return prev[curr];
+    }, obj);
+  }
+
+  // person.friends.0.name
+  // person.nicknames.0
+  function insertSafe(obj, paths, value) {
+    let parentRoot;
+    let root = obj;
+    const key = paths.pop();
+
+    for (const i in paths) {
+      const path = paths[i];
+      if (path == "0" && !Array.isArray(root)) {
+        root = {};
+        parentRoot[paths[i - 1]] = [root];
+      } else {
+        if (!(path in root))
+          root[path] = {};
+        parentRoot = root;
+        root = root[path]; 
+      }
+    }
+
+    if (!Array.isArray(root) || key in root)
+      root[key] = value;
+    else
+      root.push(value);
+    return obj;
+  }
+
+  const base = {
+    wrapType(el, value) {
+      switch (el.getAttribute(DATA_TYPE)) {
+        case "number":
+          return value * 1;
+        default:
+          return value;
+      }
+    },
+    getNumber(el, value) {
+      if (value === null || value === undefined)
+        value = el.getAttribute('defaultValue');
+      if (value === null || value === undefined)
+        value = el.getAttribute('min');
+      return value === null || value === undefined ? '' : value;
+    },
+    specialInputTags: {
+      checkbox: { 
+        set(el, value) {
+          el.checked = (value && value != "false");
+        },
+        get(el) {
+          return el.checked;
         }
+      },
+      number: {
+        set(el, value) {
+          el.value = base.getNumber(el, value);
+        },
+        get (el) {
+          return el.value * 1;
+        }
+      },
+      range: {
+        set(el, value) {
+          value = base.getNumber(el, value);
+          el.value = value ? value : 0;
+        },
+        get(el) {
+          return el.value * 1;
+        }
+      }
+    },
+    specialTags: {
+      INPUT: {
+        set(el, value) {
+          const type = el.getAttribute('type');
+          if (type in base.specialInputTags)
+            base.specialInputTags[type].set(el, value);
+          else
+            el.value = value ? value : '';
+        },
+        get(el) {
+          const type = el.getAttribute('type');
+          if (type in base.specialInputTags)
+            return base.specialInputTags[type].get(el);
+          else
+            return el.value;
+        }
+      },
+      SELECT: {
+        set(el, value) {
+          el.value = value;
+        },
+        get(el) {
+          return el.value;
+        }
+      }
+    },
+    get(el, value) {
+      if (el.anonymousChildTemplate) {
+        if (el.children.length > 1) {
+          const result = [];
+          for (const child of el.children)
+            result.push(getChildObject(child));
+          return result;
+        } else {
+          return getChildObject(el.children[0]);
+        }
+      } else {
+        if (el.tagName in base.specialTags)
+          return base.wrapType(el, base.specialTags[el.tagName].get(el));
+        else
+          return base.wrapType(el, el.innerHTML);
+      }
+    },
+    set(el, value) {
+      if (el.tagName in base.specialTags)
+        base.specialTags[el.tagName].set(el, value);
+      else
+        el.innerHTML = value ? value : '';      
+    }
+  };
+
+  function appendTemplate(el, template, dataPath, objectValue, obj) {
+    if (Array.isArray(objectValue)) {
+      dataPath += '.';
+      for (let i = 0; i < objectValue.length; ++i)
+        appendTemplate(el, template.cloneNode(true), dataPath + i, objectValue[i], obj);
+    } else {
+      for (const child of template.querySelectorAll(DATA_SELECTOR))
+        child.setAttribute(DATA, `${dataPath}.${child.getAttribute(DATA)}`);
+      el.appendChild(template);
+      putObject(template, obj);
+    }
+  }
+  
+  function getTemplate(id) {
+    if (id in appScope.templates) {
+      const template = appScope.templates[id].cloneNode(true);
+      for (const selectEl of template.querySelectorAll(OPTIONS_SELECTOR)) {
+        if (selectEl.tagName == "SELECT")
+          buildDynamicOptions(selectEl, selectEl.getAttribute(OPTIONS));
+        else
+          console.error(`Cannot use options-id ${selectEl.getAttribute(OPTIONS)} for then non <select> tag: <${selectEl.tagName}>`);
+      }
+      return template;
+    } else {
+      console.error(`Template ${id} not defined`);
+    }
+  }
+
+  
+  
+  
+  
+  
+  
+  
+  function putObject(template, obj) {
+    for (const el of template.querySelectorAll(ATTRS_SELECTOR)) {
+      const dataPath = el.getAttribute('attrs');
+      setAttributes(el, resolveSafe(obj, dataPath.split('.')));
+    }
+
+    for (const el of template.querySelectorAll(DATA_SELECTOR)) {
+      const refId = el.getAttribute(CTE_REF);
+      const dataPath = el.getAttribute(DATA);
+      const objectValue = resolveSafe(obj, dataPath.split('.'));
+
+      if (refId) {
+        removeChildren(el);
+        const template = getTemplate(refId);
+        appendTemplate(el, template, dataPath, objectValue, obj);
+      } else if (Array.isArray(objectValue)) {
+        const childTemplate = el.firstChild;
+        appendTemplate(el, childTemplate, dataPath, objectValue, obj);
+      } else {
+        base.set(el, objectValue);
       }
     }
   }
 
-  function getTemplate(id) {
-    const template = appScope.templates[id];
-    return template.cloneNode(true);
+  function updateObject(template, dataObj) {
+    if (Array.isArray(dataObj))
+      console.error("Cannot handle array as object for populating a singular template");
+    else
+      putObject(template, dataObj);
   }
+
+  function getObject(template) {
+    const object = {};
+    for (const el of template.querySelectorAll(DATA_SELECTOR)) {
+      const pathAttribute = el.getAttribute(DATA);
+      if (!el.hasAttribute(CTE_REF))
+        insertSafe(object, pathAttribute.split('.'), base.get(el));
+    }
+    return object;
+  }
+
   
-  function put(template, obj) {
-    for (const el of template.querySelectorAll('[attrs]')) {
-      const dataPath = el.getAttribute('attrs');
-      populateAttributes(el, getObjectValue(obj, dataPath));
-    }
-
-    for (const el of template.querySelectorAll('[data]')) {
-      const dataPath = el.getAttribute('data');
-      const objectValue = getObjectValue(obj, dataPath);
-      if (Array.isArray(objectValue))
-        populateInnerTemplate(el, objectValue);
-      else
-        populateValue(el, objectValue);
-    }
-  }
-
+  
+  
+  
+  
   function newDom(template, container, object) {
-    put(template, object);
+    putObject(template, object);
     container.appendChild(template);
-    return template;
   }
 
   function newDomUsingTemplateId(templateId, container, object) {
     const template = getTemplate(templateId);
     if (template)
-      return newDom(template, container, object)
+      newDom(template, container, object)
     else
       console.error(`Cannot find template with id ${templateId}`);
+    return template;
   }
 
   function newDomUsingSelector(templateId, selector, objects) {
+    const result = [];
     for (const container of document.querySelectorAll(selector))
-      return newDomUsingTemplateId(templateId, container, objects);
+      result.push(newDomUsingTemplateId(templateId, container, objects));
+    if (result.length == 1)
+      return result[0];
+    return result;
   }
 
   function newDomUsingSelectorAndObjects(templateId, selector, objects) {
@@ -174,41 +319,44 @@
     }
   }
 
-  function putObject(template, dataObj) {
-    if (Array.isArray(dataObj))
-      for (const obj of dataObj)
-        put(template, obj);
-    else
-      put(template, dataObj);
-  }
+  function getChildObject(container) {
+    const object = {};
+    for (const el of container.querySelectorAll(DATA_SELECTOR)) {
+      const pathAttribute = el.getAttribute(DATA);
 
-  function getObject(template, dataObj) {
-    for (const el of document.querySelectorAll('[data]')) {
-      
+      if (!pathAttribute.startsWith(THIS))
+        continue
+
+      if (pathAttribute == THIS)
+        return base.get(el);
+      else
+        populateObject(object, pathAttribute, base.get(el));
     }
+
+    return object;
   }
 
   const lib = {
-    tools: {
-      ready,
-      createObject
-    },
     getTemplate,
     newDom: newDomUsingSelectorAndObjects,
-    putObject,
-    getObject
+    updateObject,
+    getObject,
+//    setDynamicOptions
   };
 
-  const appScope = lib.tools.createObject('com.mentalelemental.cte', window);
+  const appScope = insertSafe(window, 'com.mental-elemental.cte'.split('.'), {});
   appScope.templates = {};
 
-  lib.tools.ready(() => {
-    for (const template of document.querySelectorAll('[cte-id]')) {
-      const id = template.getAttribute('cte-id');
-      template.removeAttribute('cte-id');
+  ready(() => {
+    //let anonymousCount = 0;
+    for (const template of document.querySelectorAll(CTE_SELECTOR)) {
+      const id = template.getAttribute(CTE);
+      template.removeAttribute(CTE);
       template.parentNode.removeChild(template);
       appScope.templates[id] = template;
-    }
+
+      // TODO Generate anonymous templates using "this"
+    }    
   });
 
   window.cte = lib;
